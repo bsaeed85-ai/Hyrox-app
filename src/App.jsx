df8964f7820d98b1621f6293e94353e300e93bb2
bsaeed85-ai <b.saeed85@googlemail.com>
2025-08-30 10:42:55 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { createClient } from "@supabase/supabase-js";
+import "./styles.css";
+
 
 /* ---------- Supabase (global client) ---------- */
 const supa = (() => {
